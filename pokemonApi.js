07f562082feccfc4cfaaf015d2c0cdc9dfbf0e5b
GMkonan <guilhemont@hotmail.com>
2021-02-19 10:38:49 -0300

    added modal with better images from other API

--- pokemonApi.js
+++ pokemonApi.js
@@ -1,9 +1,13 @@
 const NumberOfPokemons = 150;
 
 const getPokemon = async (id) => {
-    await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-        .then(res => res.json())
-        .then(data => createPokeCard(data))
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    let data = await response.json()
+    createPokeCard(data);
+    createModal(data);
+    //return data;
+    /*.then(res => res.json())
+        .then(data => createPokeCard(data))*/
 }
 
 const createPokeCard = (data) => {
@@ -21,9 +25,43 @@ const createPokeCard = (data) => {
     pokemonCard.classList.add("card")
     pokemonCard.classList.add("btn")
     pokemonCard.setAttribute("data-toggle", "modal")
-    pokemonCard.setAttribute("data-target", "#exampleModal")
+    pokemonCard.setAttribute("data-target", `#${data.id}`)
     pokemonCard.innerHTML = card;
     document.getElementById("pokemon-container").appendChild(pokemonCard)
+}
+
+const createModal = (data) => {
+    let modal = `
+    <div class="modal fade" id="${data.id}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">${data.name}</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+        <div style="display:inline-block;vertical-align:top;">
+        <img src="https://pokeres.bastionbot.org/images/pokemon/${data.id}.png" width="200px" height="200px"/>
+        </div>
+        <div style="display:inline-block;">
+        <p><b>Nome:</b> ${data.name} </p>
+        <p><b>Habilidades:</b> ${data.abilities.map(pokemon => pokemon.ability.name)} </p>
+        </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>  
+</div>
+    `
+    let pokemonModal = document.createElement("div")
+    pokemonModal.innerHTML = modal;
+    document.getElementById("pokemon-container").appendChild(pokemonModal)
 }
 
 const showPokemons = async () => {
